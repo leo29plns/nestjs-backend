2e95f85aefe77adaf443f4bb2a18b10635b35fc0
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-03-11 12:03:06 +0100

    chore: removed admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      `mongodb://127.0.0.1:27017/backend?authSource=admin`,
-    ),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/backend'),
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
